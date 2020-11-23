73008b55356c53df6d105d77a99374db1056225c
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-11-23 15:54:29 -0500

    added files

---
+++ NestingofArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NestingofArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = { 5, 4, 0, 3, 1, 6, 2 };
+            ArrayNesting(nums);
+
+        }
+        public static int ArrayNesting(int[] nums)
+        {
+            HashSet<int> hset = new HashSet<int>();
+            int maxcount = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+                if(!hset.Contains(i))
+                {
+                    int count = 0;
+                    while(!hset.Contains(i))
+                    {
+                        hset.Add(i);
+                        i = nums[i];
+                        count++;
+                    }
+                    if(count> maxcount)
+                    {
+                        maxcount = count;
+                    }
+                }
+            }
+            return maxcount;
+        }
+    }
+}
